a03b7d21e4189fe850d0478c0e02af6a6a972a06
yolojewjitsu <thefirstjewjitsu@gmail.com>
2024-05-17 22:29:31 +0300

    Update API URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   const fetchWeather = async (latitude, longitude) => {
     setLoading(true);
     try {
-      const response = await axios.get(`http://localhost:3000/api/weather?lat=${latitude}&lon=${longitude}`);
+      const response = await axios.get(`https://weather-n7nk.onrender.com/api/weather?lat=${latitude}&lon=${longitude}`);
       setWeatherData(response.data);
     } catch (error) {
       setWeatherData([]);
--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': 'https://weather-n7nk.onrender.com/'
     }
   }
 });
